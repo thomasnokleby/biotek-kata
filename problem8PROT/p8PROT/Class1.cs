fd54f99c1b38d5bc0568de6f35d8ccb50c7899e9
thomasnokleby <thomas.nokleby@gmail.com>
2020-03-23 19:33:22 +0100

    Løste problem 8 PROT

---
+++ problem8PROT/p8PROT/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p8PROT
+{
+    public class Class1
+    {
+        public static string mapFromRNAToAminoAlphabet(string rna) 
+        {
+            if (rna == null || string.IsNullOrEmpty(rna)) 
+            {
+                return "";
+            }
+
+            if (rna.Length % 3 != 0) {
+                throw new Exception("DNA string length was not divisible by 3 ");
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (var i=0; i < rna.Length; i += 3) 
+            {
+                string rnaFragment = rna.Substring(i, 3);
+                var aminoLetter = RNACodonTable.RNAToAminoLetterEmptyStops[rnaFragment];
+                sb.Append(aminoLetter);
+            }
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ problem8PROT/p8PROT/RNACodonTable.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace p8PROT
+{
+    public class RNACodonTable
+    {
+        private static Dictionary<string, string> RNAToAminoLetter = new Dictionary<string, string>
+        {
+            { "UUU" , "F"}, { "CUU" , "L"},{ "AUU" , "I"},{ "GUU" , "V"},
+            { "UUC" , "F"}, { "CUC" , "L"},{ "AUC" , "I"},{ "GUC" , "V"},
+            { "UUA" , "L"}, { "CUA" , "L"},{ "AUA" , "I"},{ "GUA" , "V"},
+            { "UUG" , "L"}, { "CUG" , "L"},{ "AUG" , "M"},{ "GUG" , "V"},
+            { "UCU" , "S"}, { "CCU" , "P"},{ "ACU" , "T"},{ "GCU" , "A"},
+            { "UCC" , "S"}, { "CCC" , "P"},{ "ACC" , "T"},{ "GCC" , "A"},
+            { "UCA" , "S"}, { "CCA" , "P"},{ "ACA" , "T"},{ "GCA" , "A"},
+            { "UCG" , "S"}, { "CCG" , "P"},{ "ACG" , "T"},{ "GCG" , "A"},
+            { "UAU" , "Y"}, { "CAU" , "H"},{ "AAU" , "N"},{ "GAU" , "D"},
+            { "UAC" , "Y"}, { "CAC" , "H"},{ "AAC" , "N"},{ "GAC" , "D"},
+            { "UAA" , "Stop"}, { "CAA" , "Q"},{ "AAA" , "K"},{ "GAA" , "E"},
+            { "UAG" , "Stop"}, { "CAG" , "Q"},{ "AAG" , "K"},{ "GAG" , "E"},
+            { "UGU" , "C"}, { "CGU" , "R"},{ "AGU" , "S"},{ "GGU" , "G"},
+            { "UGC" , "C"}, { "CGC" , "R"},{ "AGC" , "S"},{ "GGC" , "G"},
+            { "UGA" , "Stop"}, { "CGA" , "R"},{ "AGA" , "R"},{ "GGA" , "G"},
+            { "UGG" , "W"}, { "CGG" , "R"},{ "AGG" , "R"},{ "GGG" , "G"}
+        };
+
+        public static Dictionary<string, string> RNAToAminoLetterEmptyStops = new Dictionary<string, string>
+        {
+            { "UUU" , "F"}, { "CUU" , "L"},{ "AUU" , "I"},{ "GUU" , "V"},
+            { "UUC" , "F"}, { "CUC" , "L"},{ "AUC" , "I"},{ "GUC" , "V"},
+            { "UUA" , "L"}, { "CUA" , "L"},{ "AUA" , "I"},{ "GUA" , "V"},
+            { "UUG" , "L"}, { "CUG" , "L"},{ "AUG" , "M"},{ "GUG" , "V"},
+            { "UCU" , "S"}, { "CCU" , "P"},{ "ACU" , "T"},{ "GCU" , "A"},
+            { "UCC" , "S"}, { "CCC" , "P"},{ "ACC" , "T"},{ "GCC" , "A"},
+            { "UCA" , "S"}, { "CCA" , "P"},{ "ACA" , "T"},{ "GCA" , "A"},
+            { "UCG" , "S"}, { "CCG" , "P"},{ "ACG" , "T"},{ "GCG" , "A"},
+            { "UAU" , "Y"}, { "CAU" , "H"},{ "AAU" , "N"},{ "GAU" , "D"},
+            { "UAC" , "Y"}, { "CAC" , "H"},{ "AAC" , "N"},{ "GAC" , "D"},
+            { "UAA" , ""}, { "CAA" , "Q"},{ "AAA" , "K"},{ "GAA" , "E"},
+            { "UAG" , ""}, { "CAG" , "Q"},{ "AAG" , "K"},{ "GAG" , "E"},
+            { "UGU" , "C"}, { "CGU" , "R"},{ "AGU" , "S"},{ "GGU" , "G"},
+            { "UGC" , "C"}, { "CGC" , "R"},{ "AGC" , "S"},{ "GGC" , "G"},
+            { "UGA" , ""}, { "CGA" , "R"},{ "AGA" , "R"},{ "GGA" , "G"},
+            { "UGG" , "W"}, { "CGG" , "R"},{ "AGG" , "R"},{ "GGG" , "G"}
+        };
+
+    }
+}
+
+/**
+ UUU F      CUU L      AUU I      GUU V
+UUC F      CUC L      AUC I      GUC V
+UUA L      CUA L      AUA I      GUA V
+UUG L      CUG L      AUG M      GUG V
+UCU S      CCU P      ACU T      GCU A
+UCC S      CCC P      ACC T      GCC A
+UCA S      CCA P      ACA T      GCA A
+UCG S      CCG P      ACG T      GCG A
+UAU Y      CAU H      AAU N      GAU D
+UAC Y      CAC H      AAC N      GAC D
+UAA Stop   CAA Q      AAA K      GAA E
+UAG Stop   CAG Q      AAG K      GAG E
+UGU C      CGU R      AGU S      GGU G
+UGC C      CGC R      AGC S      GGC G
+UGA Stop   CGA R      AGA R      GGA G
+UGG W      CGG R      AGG R      GGG G 
+     
+     */
